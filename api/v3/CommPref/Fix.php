c64df183ded7cb949ae1c9926d6769cb3b59dacc
ErikHommel <erik.hommel@civicoop.org>
2018-12-06 11:11:34 +0100

    fix comm methods after 5.5 upgrade

---
+++ api/v3/CommPref/Fix.php
@@ -0,0 +1,31 @@
+<?php
+use CRM_Corrections_ExtensionUtil as E;
+
+
+/**
+ * CommPref.Fix API
+ *
+ * One Time Scheduled Job to fix the Preferred Communicaiton Methods in civicrm_contact that
+ * are stored without VALUE SEPARATOR
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_comm_pref_Fix($params) {
+  $count = 0;
+  $query = "SELECT id, preferred_communication_method FROM civicrm_contact WHERE length(preferred_communication_method) = %1";
+  $dao = CRM_Core_DAO::executeQuery($query, [1 => [1, 'Integer']]);
+  while ($dao->fetch()) {
+    $new = CRM_Core_DAO::VALUE_SEPARATOR . $dao->preferred_communication_method . CRM_Core_DAO::VALUE_SEPARATOR;
+    $update = "UPDATE civicrm_contact SET preferred_communication_method = %1 WHERE id = %2";
+    CRM_Core_DAO::executeQuery($update, [
+      1 => [$new, 'String'],
+      2 => [$dao->id, 'Integer'],
+    ]);
+    $count++;
+  }
+  return civicrm_api3_create_success([$count . ' communication methods fixed'], $params, 'CommPref', 'Fix');
+}
